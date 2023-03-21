6afef304fbf61eecf2225c525a614a5344ffa38a
67anna <@DESKTOP-VICLL7Q.(none)>
2023-03-21 20:44:49 +0300

    inital commit

---
+++ task1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int A, B;
+Console.Write("Введите А: ");
+int.TryParse(Console.ReadLine(), out A);
+Console.Write("Введите B: ");
+int.TryParse(Console.ReadLine(), out B);
+
+Console.Write($"{A}, {B} -> {Exponent(A, B)}");
+
+int Exponent(int a, int b)
+{
+    int aa = a;
+    for (int i = 2; i <= b; i++)
+        aa *= a;
+    return aa;
+}
---
+++ task27/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int A;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out A);
+
+Console.Write($"{A} -> {Sum(A)}");
+
+int Sum(int a)
+{
+    int aa = a;
+    int sum = 0;
+    while (aa > 0)
+    {
+        sum = sum + ( aa % 10);
+        aa = aa / 10;
+    }
+    return sum;
+}
+
+// int aa = A;
+// int sum = 0;
+// while (aa > 0)
+// {
+//     sum = sum + ( aa % 10);
+//     Console.WriteLine(aa);
+//     aa = aa / 10;
+//     Console.WriteLine(aa);
+// }
+// Console.Write(aa);
